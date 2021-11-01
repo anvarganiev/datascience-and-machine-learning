436c974ccfa75645be4e857b004168ff3b84dc58
Anvar Ganiev <qwerty0704996@gmail.com>
2021-11-01 22:46:25 +0300

    added style transfer model

---
+++ style_transfer/style_transfer.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+
+import torch
+
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+from losses import run_style_transfer
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Loading the Images
+imsize = 512 if torch.cuda.is_available() else 256  # use small size if no gpu
+loader = transforms.Compose([
+    transforms.Resize((imsize, imsize)),  # scale imported image
+    transforms.ToTensor()])  # transform it into a torch tensor
+
+
+def image_loader(image_name):
+    image = Image.open(image_name)
+    # fake batch dimension required to fit network's input dimensions
+    image = loader(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+
+style_img = image_loader(
+    "/Users/ganievanvar/PycharmProjects/PyTorch/style_transfer/data/images/neural_style/munk.jpg")
+
+content_img = image_loader("/Users/ganievanvar/PycharmProjects/PyTorch/style_transfer/data/images/neural_style"
+                           "/dancing.jpg")
+
+print(style_img.size(), content_img.size())
+
+assert style_img.size() == content_img.size(), \
+    "we need to import style and content images of the same size"
+
+unloader = transforms.ToPILImage()  # reconvert into PIL image
+
+plt.ion()
+
+
+def imshow(tensor, title=None):
+    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
+    image = image.squeeze(0)  # remove the fake batch dimension
+    image = unloader(image)
+    plt.imshow(image)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+plt.figure()
+imshow(style_img, title='Style Image')
+
+plt.figure()
+imshow(content_img, title='Content Image')
+
+# Importing the Model
+
+cnn = models.vgg19(pretrained=True).features.to(device).eval()
+
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+input_img = content_img.clone()
+# if you want to use white noise instead uncomment the below line:
+# input_img = torch.randn(content_img.data.size(), device=device)
+
+# add the original input image to the figure:
+plt.figure()
+imshow(input_img, title='Input Image')
+
+output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
+                            content_img, style_img, input_img)
+
+plt.figure()
+imshow(output, title='Output Image')
+
+plt.ioff()
+plt.show()
