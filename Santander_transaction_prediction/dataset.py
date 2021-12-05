774b989ab1a1d0df29249311e0bcbe1c25657d69
Anvar Ganiev <qwerty0704996@gmail.com>
2021-12-05 17:35:44 +0300

    added kaggle solution

---
+++ Santander_transaction_prediction/dataset.py
@@ -0,0 +1,25 @@
+from math import ceil
+
+import pandas as pd
+import torch
+from torch.utils.data import TensorDataset
+from torch.utils.data.dataset import random_split
+
+
+def get_data():
+    train_data = pd.read_csv("../data/santander/new_train.csv")
+    y = train_data["target"]
+    X = train_data.drop(["ID_code", "target"], axis=1)
+    X_tensor = torch.tensor(X.values, dtype=torch.float32)
+    y_tensor = torch.tensor(y.values, dtype=torch.float32)
+    ds = TensorDataset(X_tensor, y_tensor)
+    train_ds, val_ds = random_split(ds, [int(0.99 * len(ds)), ceil(0.01 * len(ds))])
+
+    test_data = pd.read_csv("../data/santander/new_test.csv")
+    test_ids = test_data["ID_code"]
+    X = test_data.drop(["ID_code"], axis=1)
+    X_tensor = torch.tensor(X.values, dtype=torch.float32)
+    y_tensor = torch.tensor(y.values, dtype=torch.float32)
+    test_ds = TensorDataset(X_tensor, y_tensor)
+
+    return train_ds, val_ds, test_ds, test_ids
